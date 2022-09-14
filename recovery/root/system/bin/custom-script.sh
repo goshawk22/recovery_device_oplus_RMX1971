c944621f950ef22739e5bd42ddf69f58a0434d0a
Adam Lawson <dev@goshawk22.uk>
2022-09-14 21:01:43 +0100

    RMX1971: Reset all props for RMX1971
    Wasn't properly setting props which was causing some RUI1 roms to fail at flashing

--- recovery/root/system/bin/custom-script.sh
+++ recovery/root/system/bin/custom-script.sh
@@ -14,18 +14,8 @@ else
     resetprop ro.device.latest_fw false
 fi
 
-if grep -q SDM710 /proc/cpuinfo && [ $FP_ID = F_1511 ]; then
-  setprop ro.boot.prjname 18621
-  resetprop ro.build.product RMX1851
-elif grep -q SDM712 /proc/cpuinfo && [ $FP_ID = F_1511 ]; then
-  setprop ro.boot.prjname 19691
-  resetprop ro.build.product RMX1971
-elif grep -q 19651 /proc/cmdline; then
-  setprop ro.boot.prjname 19651
-  resetprop ro.build.product RMX1921
-elif grep -q 18041 /proc/cmdline && [ $FP_ID = G_OPTICAL_G2 ] ; then
-  setprop ro.boot.prjname 19605
-  resetprop ro.build.product RMX1901
-fi
-
-resetprop ro.product.product.device $(getprop ro.build.product)
+setprop ro.boot.prjname 19691
+resetprop ro.build.product RMX1971
+resetprop ro.product.product.device RMX1971
+resetprop ro.product.device RMX1971
+resetprop ro.product.name RMX1971
